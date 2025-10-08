23ab190b40305ea1125833c968563a31890925c4
Tyson Nguyen <117993365+TysonNguyen01142004@users.noreply.github.com>
2025-10-08 15:15:02 +0300

    Create run_test.py

---
+++ run_test.py
@@ -0,0 +1,41 @@
+from method import main  # replace with your filename
+
+# SIMP
+main(
+    nelx=60,  # number of elements in x-direction
+    nely=30,  # number of elements in y-direction
+    volfrac=0.5,  # target volume fraction
+    penal=3.0,  # penalization factor
+    rmin=1.5,  # filter radius
+    ft=1,  # filter type: 1=density filter, 0=sensitivity filter
+    xsolv=0,  # optimizer: 0=OC, 1=MMA
+    method="SIMP",  # choose SIMP method
+)
+
+
+# RAMP:
+main(
+    nelx=60,
+    nely=30,
+    volfrac=0.5,
+    penal=3.0,
+    rmin=1.5,
+    ft=1,
+    xsolv=0,
+    method="RAMP",
+    ramp_q=0.5,
+)
+
+
+# BESO
+main(
+    nelx=60,
+    nely=30,
+    volfrac=0.5,
+    penal=3.0,
+    rmin=1.5,
+    ft=1,
+    xsolv=0,
+    method="BESO",
+    b_erase=0.02,
+)
